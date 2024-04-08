b265bf4e0b57c31c082d57cb5d7119bf8a07c8e6
IrCamel <dario03juve@gmail.com>
2024-04-08 11:47:40 +0200

    db json & navbar

--- presidentProject/src/app/app.module.ts
+++ presidentProject/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NavbarComponent } from './main-component/navbar/navbar.component';
+import { FooterComponent } from './main-component/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
