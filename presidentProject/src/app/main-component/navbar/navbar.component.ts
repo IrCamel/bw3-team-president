cf3300688c26ae4e7cdfbe48b3950e73b04d1fb5
FedericoPecoraro <federico.pecoraro8@gmail.com>
2024-04-08 17:57:39 +0200

    navbar

--- presidentProject/src/app/main-component/navbar/navbar.component.ts
+++ presidentProject/src/app/main-component/navbar/navbar.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
-
+	isMenuCollapsed = true;
 }
--- presidentProject/src/app/pages/categories/categories-routing.module.ts
+++ presidentProject/src/app/pages/categories/categories-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CategoriesComponent } from './categories.component';
+import { MarvelComponent } from './marvel/marvel.component';
+import { DcComponent } from './dc/dc.component';
+import { HarryPotterComponent } from './harry-potter/harry-potter.component';
+import { StarWarsComponent } from './star-wars/star-wars.component';
+import { SignoreDegliAnelliComponent } from './signore-degli-anelli/signore-degli-anelli.component';
 
-const routes: Routes = [{ path: '', component: CategoriesComponent }];
+const routes: Routes = [
+  { path: '', component: CategoriesComponent },
+  { path: 'marvel', component: MarvelComponent },
+  { path: 'dc', component: DcComponent },
+  { path: 'harry-potter', component: HarryPotterComponent },
+  { path: 'star-wars', component: StarWarsComponent },
+  { path: 'signore-degli-anelli', component: SignoreDegliAnelliComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
