86fb6e3e340f9ee8d89a823861d64ccba2f93e9f
iosonodipe <dipecod@gmail.com>
2024-04-09 10:09:03 +0200

    login e register

--- presidentProject/src/app/pages/auth/login/login.component.ts
+++ presidentProject/src/app/pages/auth/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { iLoginData } from '../../../models/login-data';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  loginData: iLoginData = {email: '', password: ''}
+
+  constructor(
+    private authSvc:AuthService,
+    private router:Router
+    ){}
+
+    signIn(){
+      this.authSvc.login(this.loginData)
+      .subscribe(data => {
+        this.router.navigate(['/userCart'])
+      })
+    }
 
 }
--- presidentProject/src/environments/environment.development.ts
+++ presidentProject/src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
   userUrl: "http://localhost:3000/users",
   productUrl: "http://localhost:3000/product",
+  registerUrl: "http://localhost:3000/register",
+  loginUrl: "http://localhost:3000/login",
 };
