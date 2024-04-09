e97f61194978eded294cb5f1e9ed223af327c5d1
BuBiNo-MoB <emanuel.incarnato@gmail.com>
2024-04-09 11:02:23 +0200

    Categorie appoooooosto

--- presidentProject/src/app/pages/categories/signore-degli-anelli/signore-degli-anelli.component.ts
+++ presidentProject/src/app/pages/categories/signore-degli-anelli/signore-degli-anelli.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CategoriesService } from '../categories.service';
+import { iProduct } from '../../../models/product';
 
 @Component({
   selector: 'app-signore-degli-anelli',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './signore-degli-anelli.component.scss'
 })
 export class SignoreDegliAnelliComponent {
+  lotProducts: iProduct[] = [];
 
+  constructor(private categoriesService: CategoriesService) { }
+
+  ngOnInit(): void {
+    this.categoriesService.getProductsByCategory('Il Signore degli Anelli').subscribe(products => {
+      this.lotProducts = products;
+    });
+  }
 }
