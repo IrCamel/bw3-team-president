f43917594d9259b487f2c788c2feb8cc50664899
BuBiNo-MoB <emanuel.incarnato@gmail.com>
2024-04-10 16:41:32 +0200

    product-details funzionante

--- presidentProject/src/app/services/product.service.ts
+++ presidentProject/src/app/services/product.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.development';
 import { iProduct } from '../models/product';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Injectable({
   providedIn: 'root'
@@ -19,5 +20,8 @@ export class ProductService {
     return this.http.get<iProduct[]>(this.productUrl)
   }
 
-
+  getProductById(id: number): Observable<iProduct> {
+    const url = `${this.productUrl}/${id}`;
+    return this.http.get<iProduct>(url);
+  }
 }
