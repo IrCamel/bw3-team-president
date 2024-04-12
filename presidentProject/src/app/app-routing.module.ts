613f6e5aa13a253993ffe7633e8748d79f3de5d8
Lorepelhu98 <lorenzopelhuri@libero.it>
2024-04-12 13:12:32 +0200

    commit riepilogo

--- presidentProject/src/app/app-routing.module.ts
+++ presidentProject/src/app/app-routing.module.ts
@@ -2,40 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '',
-    loadChildren: () => import('./pages/landing-page/landing-page.module').then(m => m.LandingPageModule)
-  },
-  { path: 'auth',
-    loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule)
-  },
-  { path: 'productDetails',
-    loadChildren: () => import('./pages/product-details/product-details.module').then(m => m.ProductDetailsModule)
-  },
-  { path: 'categories',
-    loadChildren: () => import('./pages/categories/categories.module').then(m => m.CategoriesModule)
-  },
-  { path: 'userCart',
-    loadChildren: () => import('./pages/user-cart/user-cart.module').then(m => m.UserCartModule)
-  },
-  { path: 'info',
-    loadChildren: () => import('./pages/info/info.module').then(m => m.InfoModule)
-  },
-  { path: '**',
-    loadChildren: () => import('./pages/error404/error404.module').then(m => m.Error404Module)
-  },
-  { path: 'productDetails',
-    loadChildren: () => import('./pages/product-details/product-details.module').then(m => m.ProductDetailsModule)
-  },
-  { path: 'categories',
-    loadChildren: () => import('./pages/categories/categories.module').then(m => m.CategoriesModule)
-  },
-  { path: 'userCart',
-    loadChildren: () => import('./pages/user-cart/user-cart.module').then(m => m.UserCartModule)
-  },
-  { path: 'info',
-    loadChildren: () => import('./pages/info/info.module').then(m => m.InfoModule)
-  },
+  { path: '', loadChildren: () => import('./pages/landing-page/landing-page.module').then(m => m.LandingPageModule) },
+  { path: 'auth', loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule) },
+  { path: 'productDetails', loadChildren: () => import('./pages/product-details/product-details.module').then(m => m.ProductDetailsModule) },
+  { path: 'categories', loadChildren: () => import('./pages/categories/categories.module').then(m => m.CategoriesModule) },
+  { path: 'userCart', loadChildren: () => import('./pages/user-cart/user-cart.module').then(m => m.UserCartModule) },
+  { path: 'info', loadChildren: () => import('./pages/info/info.module').then(m => m.InfoModule) },
+  { path: '**', loadChildren: () => import('./pages/error404/error404.module').then(m => m.Error404Module) }
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- presidentProject/src/app/pages/user-cart/user-cart-routing.module.ts
+++ presidentProject/src/app/pages/user-cart/user-cart-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserCartComponent } from './user-cart.component';
+import { RiepilogoComponent } from './riepilogo/riepilogo.component';
 
-const routes: Routes = [{ path: '', component: UserCartComponent }];
+const routes: Routes = [{ path: '', component: UserCartComponent },
+{ path: 'riepilogo', component: RiepilogoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
